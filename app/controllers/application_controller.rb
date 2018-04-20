6a5e85aa96ef15b828515df274f7fc15da559598
Riemann <dr.mactt@gmail.com>
2018-04-20 23:28:22 +0800

    add column to devise sign up and edit page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  protected
+  # 加上自訂欄位到 Devise 的註冊和編輯頁面
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :avatar, :role, :intro])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :avatar, :role, :intro])
+  end
 end
