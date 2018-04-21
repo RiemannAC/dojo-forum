8c6399e4006702838bccb5158c36af3856c6bdcc
Riemann <dr.mactt@gmail.com>
2018-04-22 03:05:04 +0800

    fix devise sign up Unpermitted parameters problem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!, except: [:index]
-
-  protected
-  # 加上自訂欄位到 Devise 的註冊和編輯頁面
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :avatar, :role, :intro])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :avatar, :role, :intro])
-  end
+  # devise：user 新增欄位給值
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
@@ -16,5 +11,11 @@ class ApplicationController < ActionController::Base
       flash[:alert] = "Not allow!"
       redirect_to root_path
     end
+  end
+
+  # 加上自訂欄位到 Devise 的註冊和編輯頁面
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :avatar, :role, :intro])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :avatar, :role, :intro])
   end
 end
