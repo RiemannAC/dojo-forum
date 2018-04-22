2f1f671c6a7227dcbece95d41cb950ac3ee8451f
Riemann <dr.mactt@gmail.com>
2018-04-22 20:59:43 +0800

    setup seed data for default categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Category
+
+Category.destroy_all
+
+category_list = [
+  { name: "Cat" },
+  { name: "Dog" },
+  { name: "Hamster" }
+]
+
+category_list.each do |category|
+  Category.create( name: category[:name] )
+end
+puts "Category created!"
+
+# admin user
+
 User.create(name: "Admin", email: "admin@example.com", password: "12345678", role: "admin")
 puts "Admin user created!"
