95f8afd6bc079320e7fcfc249d87d0ff43dd2dd1
Riemann <dr.mactt@gmail.com>
2018-04-29 01:43:22 +0800

    add #index and index page to feeds

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,2 +1,9 @@
 class FeedsController < ApplicationController
+  def index
+    @user_count = User.all.count
+    @post_count = Post.are_public?.count
+    @comment_count = Comment.all.count
+    @users = User.select(:id, :name, :comments_count).order(comments_count: :desc).limit(10)
+    @posts = Post.select(:id, :title, :comments_count, :updated_at).are_public?.order(comments_count: :desc).limit(10)
+  end
 end
