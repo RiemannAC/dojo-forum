22c3dc88d591a3d6d2aa4714d103d5ff4b55bd66
Riemann <dr.mactt@gmail.com>
2018-04-29 17:02:03 +0800

    add my_comment page to user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,12 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show]
+  before_action :set_user, only: [:show, :comments]
 
   def show
     @posts = @user.posts.are_viewable?(current_user).are_public?
+  end
+
+  def comments
+    @comments = @user.comments.includes(:post)
   end
 
   private
