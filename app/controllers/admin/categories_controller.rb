5f23bc5f96e49d4e1b4bf9f702ad7498c2c50ae2
Riemann <dr.mactt@gmail.com>
2018-04-30 20:28:35 +0800

    add #index and index page for admin categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,5 +1,11 @@
 class Admin::CategoriesController < Admin::BaseController
 
   def index
+    @categories = Category.all
+    if params[:id]
+      @category = Category.find(params[:id])
+    else
+      @category = Category.new
+    end
   end
 end
