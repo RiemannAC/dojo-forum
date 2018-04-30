d89a45b4b71335433daf627ab9c129e64cb7b714
Riemann <dr.mactt@gmail.com>
2018-04-30 20:37:51 +0800

    add #create to admin categories_controller and validate to category model

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -8,4 +8,22 @@ class Admin::CategoriesController < Admin::BaseController
       @category = Category.new
     end
   end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "Category was successfully created"
+      redirect_to admin_categories_path
+    else
+      @categories = Category.all
+      render :index
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ApplicationRecord
+  validates_presence_of :name
+
   has_many :category_posts, dependent: :restrict_with_error
   has_many :posts, through: :category_posts
 
