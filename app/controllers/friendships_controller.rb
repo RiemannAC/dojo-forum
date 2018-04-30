adc7b38cd78392b71e02cbb1065ec2604e151090
Riemann <dr.mactt@gmail.com>
2018-04-30 10:54:51 +0800

    add #create to friendships_controller

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,2 +1,14 @@
 class FriendshipsController < ApplicationController
+
+  def create
+    @user = User.find(params[:friend_id])
+    @friendship = current_user.request_friendships.build(friend_id: params[:friend_id])
+    if @friendship.save
+      flash[:notice] = "Successfully followed"
+      redirect_back(fallback_location: root_path)
+    else
+      flash[:alert] = @friendship.errors.full_messages.to_sentence
+      redirect_back(fallback_location: root_path)
+    end
+  end
 end
