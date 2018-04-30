a5f92062b2180337c59a9b7608f4fbd9af925350
Riemann <dr.mactt@gmail.com>
2018-04-30 21:46:52 +0800

    add #update to admin users_controller

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,4 +2,12 @@ class Admin::UsersController < Admin::BaseController
   def index
     @users = User.all
   end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(role: params[:role])
+    flash[:notice] = "#{@user.name}'s role has changed to #{params[:role]}"
+    redirect_to admin_users_path
+  end
+
 end
