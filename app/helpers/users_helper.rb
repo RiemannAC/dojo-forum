f640fcdc48d1d81f5348943adff50a056be00e5c
Riemann <dr.mactt@gmail.com>
2018-06-06 09:20:51 +0800

    add style float_r_b to user_profile button

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,4 +4,10 @@ module UsersHelper
       render partial: "shared/user_item", locals: { user: user }
     end
   end
+
+  # user_info 若顯示按鈕 => return true
+  def user_button?(user)
+    return true if user == current_user || !current_user.inverse_request_friend?(user)
+  end
+
 end
